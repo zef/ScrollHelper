78d51686e7ee551f1a3df22b079d78eab2348904
Zef Houssney <zef@madebykiwi.com>
2014-10-07 23:51:12 -0600

    Initial Commit

---
+++ ScrollHelpers/ScrollHelpers.h
@@ -0,0 +1,19 @@
+//
+//  ScrollHelpers.h
+//  ScrollHelpers
+//
+//  Created by Josef Houssney on 10/7/14.
+//  Copyright (c) 2014 Made by Kiwi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ScrollHelpers.
+FOUNDATION_EXPORT double ScrollHelpersVersionNumber;
+
+//! Project version string for ScrollHelpers.
+FOUNDATION_EXPORT const unsigned char ScrollHelpersVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ScrollHelpers/PublicHeader.h>
+
+
