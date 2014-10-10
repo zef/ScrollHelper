c09180d62a3fcbef1a31e1f5374f868243589434
Zef Houssney <zef@madebykiwi.com>
2014-10-10 14:23:34 -0600

    Add some tests.

--- ScrollHelpersTests/ScrollHelpersTests.swift
+++ ScrollHelpersTests/ScrollHelpersTests.swift
@@ -2,18 +2,21 @@
 //  ScrollHelpersTests.swift
 //  ScrollHelpersTests
 //
-//  Created by Josef Houssney on 10/7/14.
-//  Copyright (c) 2014 Made by Kiwi. All rights reserved.
+//  Created by Zef Houssney on 10/7/14.
 //
 
 import UIKit
 import XCTest
 
 class ScrollHelpersTests: XCTestCase {
-    
+        var helper = ScrollHelper()
+
     override func setUp() {
         super.setUp()
         // Put setup code here. This method is called before the invocation of each test method in the class.
+        helper.targetOffset = 100
+        helper.triggerDistance = helper.targetOffset/2
+        helper.direction = .All
     }
     
     override func tearDown() {
@@ -21,16 +24,58 @@ class ScrollHelpersTests: XCTestCase {
         super.tearDown()
     }
     
-    func testExample() {
-        // This is an example of a functional test case.
-        XCTAssert(true, "Pass")
+    func testDistance() {
+        helper.offset = 0
+        XCTAssertEqual(helper.distanceToTarget(), -100)
+
+        helper.offset = 25
+        XCTAssertEqual(helper.distanceToTarget(), -75)
+
+        helper.offset = 100
+        XCTAssertEqual(helper.distanceToTarget(), 0)
+
+        helper.offset = 125
+        XCTAssertEqual(helper.distanceToTarget(), 25)
+    }
+
+    func testPercentageForBothDirections() {
+        helper.offset = 0
+        XCTAssertEqual(helper.percentage(), 0)
+
+        helper.offset = 90
+        XCTAssertEqualWithAccuracy(helper.percentage(), 0.8, 0.001)
+
+        helper.offset = 100
+        XCTAssertEqual(helper.percentage(), 1)
+
+        helper.offset = 110
+        XCTAssertEqualWithAccuracy(helper.percentage(), 0.8, 0.001)
+
+        helper.offset = 150
+        XCTAssertEqual(helper.percentage(), 0)
+
+        helper.offset = 1000
+        XCTAssertEqual(helper.percentage(), 0)
+    }
+
+    func testPercentageForIn() {
+        helper.direction = .In
+        helper.offset = 0
+        XCTAssertEqual(helper.percentage(), 0)
+
+        helper.offset = 75
+        XCTAssertEqual(helper.percentage(), 0.5)
+
+        helper.offset = 100
+        XCTAssertEqual(helper.percentage(), 1)
+
+        helper.offset = 125
+        XCTAssertEqual(helper.percentage(), 1)
+
+        helper.offset = 1000
+        XCTAssertEqual(helper.percentage(), 1)
     }
     
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measureBlock() {
-            // Put the code you want to measure the time of here.
-        }
     }
     
 }
