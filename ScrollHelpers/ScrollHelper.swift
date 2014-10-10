f71f328c98634da926052066bb126c759407fa6d
Zef Houssney <zef@madebykiwi.com>
2014-10-10 14:28:45 -0600

    Add reversePercentage, individual trigger distances, directional arguments, and previousOffset.

--- ScrollHelpers/ScrollHelper.swift
+++ ScrollHelpers/ScrollHelper.swift
@@ -2,6 +2,16 @@
 //  ScrollHelper.swift
 //  ScrollTriggers
 //
+//  Created by Zef Houssney on 10/7/14.
+//
+
+import UIKit
+import Foundation
+
+//
+//  ScrollHelper.swift
+//  ScrollTriggers
+//
 //  Created by Josef Houssney on 10/7/14.
 //  Copyright (c) 2014 Made By Kiwi. All rights reserved.
 //
@@ -11,39 +21,70 @@ import Foundation
 
 struct ScrollHelper {
     enum Direction {
-        case Arriving, Leaving, All
+        case In, Out, All
+        // just realized could add these with trigger distances too.
+        // Something like this:
+        // case InAt(CGFloat)
+        // case OutAt(CGFloat)
+        // case AllAt(CGFloat, CGFloat)
+        // I was hoping to use the same name with an optional arg,
+        // but that doesn't seem to be supported
     }
 
-    var offset: Float = 0
-    var targetOffset: Float = 0
+    private(set) var previousOffset: CGFloat = 0
+
+    var offset: CGFloat = 0  {
+        willSet {
+            previousOffset = offset
+        }
+    }
+    var targetOffset: CGFloat = 0
     var direction = Direction.All
 
-    var triggerDistance: Float = 0
+    // should be write only or use a function or something
+    // don't have time to figure it out right now...
+    var triggerDistance: CGFloat? = 0 {
+        didSet {
+            if let distance = triggerDistance? {
+                triggerDistanceIn = distance
+                triggerDistanceOut = distance
+            }
+        }
+    }
+    var triggerDistanceIn: CGFloat = 0
+    var triggerDistanceOut: CGFloat = 0
 
     // returns the number of points from the target location
     // negative numbers indicate that the target has not been reached
     // positive numbers indicate the distance past the target
-    func distanceToTarget() -> Float {
+    func distanceToTarget() -> CGFloat {
         return offset - targetOffset
     }
 
-    func percentage() -> CGFloat {
+    func percentage(_ customDirection: Direction? = nil) -> CGFloat {
+        var direction = customDirection == nil ? self.direction : customDirection!
         var distance = distanceToTarget()
-        var percentage = CGFloat(1.0 - (abs(distance)/triggerDistance))
+
+        var percentage = CGFloat(0)
+        if distance < 0 {
+            percentage = CGFloat(1.0 - (abs(distance)/triggerDistanceIn))
+        } else {
+            percentage = CGFloat(1.0 - (abs(distance)/triggerDistanceOut))
+        }
+
         percentage = max(percentage, 0)
 
-        println(distance)
-        println(triggerDistance)
-        println(percentage)
-
         switch direction {
-        case .Arriving:
+        case .In:
             return distance > 0.0 ? 1 : percentage
-        case .Leaving:
+        case .Out:
             return distance < 0.0 ? 1 : percentage
         case .All:
             return percentage
         }
     }
 
+    func reversePercentage(_ customDirection: Direction? = nil) -> CGFloat {
+        return 1 - percentage(customDirection)
+    }
 }
--- ScrollHelpersTests/ScrollHelpersTests.swift
+++ ScrollHelpersTests/ScrollHelpersTests.swift
@@ -76,6 +76,62 @@ class ScrollHelpersTests: XCTestCase {
         XCTAssertEqual(helper.percentage(), 1)
     }
     
+    func testPercentageForOut() {
+        helper.direction = .Out
+        helper.offset = 0
+        XCTAssertEqual(helper.percentage(), 1)
+
+        helper.offset = 75
+        XCTAssertEqual(helper.percentage(), 1)
+
+        helper.offset = 100
+        XCTAssertEqual(helper.percentage(), 1)
+
+        helper.offset = 125
+        XCTAssertEqual(helper.percentage(), 0.5)
+
+        helper.offset = 150
+        XCTAssertEqual(helper.percentage(), 0)
+
+        helper.offset = 1000
+        XCTAssertEqual(helper.percentage(), 0)
     }
-    
+
+    func testReversePercentage() {
+        helper.offset = 90
+        XCTAssertEqualWithAccuracy(helper.reversePercentage(), 0.2, 0.001)
+    }
+
+    func testDirectionParamInPercentage() {
+        helper.offset = 125
+        XCTAssertEqual(helper.percentage(), 0.5)
+        XCTAssertEqual(helper.percentage(.All), 0.5)
+        XCTAssertEqual(helper.percentage(.Out), 0.5)
+
+        XCTAssertEqual(helper.percentage(.In), 1)
+        XCTAssertEqual(helper.reversePercentage(.In), 0)
+    }
+
+    func testDirectionalTriggerDistance() {
+        helper.triggerDistanceIn = 50
+        helper.triggerDistanceOut = 100
+
+//        XCTAssertNil(helper.triggerDistance?, "Trigger distance should be nil if in and out are different")
+//        helper.triggerDistanceOut = 50
+//        XCTAssertEqual(helper.triggerDistance!, 50, "Trigger distance should be set if in and out are equal")
+
+        helper.offset = 90
+        XCTAssertEqualWithAccuracy(helper.percentage(), 0.8, 0.001)
+
+        helper.offset = 110
+        XCTAssertEqualWithAccuracy(helper.percentage(), 0.9, 0.001)
+
+    }
+
+//    func testPerformanceExample() {
+//        // This is an example of a performance test case.
+////        self.measureBlock() {
+////            // Put the code you want to measure the time of here.
+////        }
+//    }
 }
